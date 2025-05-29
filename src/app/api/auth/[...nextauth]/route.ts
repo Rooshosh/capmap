08d6c6089fe8bb53bcaecc1889830ac7106c7e82
Henrik Reusch <henrikreusch@gmail.com>
2025-05-29 14:45:59 +0200

    strava signin connected to db

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,19 +1,34 @@
 import NextAuth from "next-auth"
 import Strava from "next-auth/providers/strava"
+import { PrismaAdapter } from "@next-auth/prisma-adapter"
+import { prisma } from "@/app/lib/prisma"
 
 const handler = NextAuth({
+    adapter: PrismaAdapter(prisma),
     providers: [
         Strava({
             clientId: process.env.STRAVA_CLIENT_ID!,
             clientSecret: process.env.STRAVA_CLIENT_SECRET!,
             authorization: {
                 params: {
-                    approval_prompt: "auto",
                     scope: "profile:read_all,activity:read,activity:read_all",
+                    approval_prompt: "auto",
                 }, 
             },
         }),
     ],
+    session: {
+        strategy: "jwt",
+    },
+    callbacks: {
+        async signIn({ user, account, profile }) {
+          // Only remove athlete field for Strava accounts
+          if (account?.provider === "strava" && account.athlete) {
+            delete account.athlete;
+          }
+          return true;
+        },
+      },
 })
 
 export { handler as GET, handler as POST }
